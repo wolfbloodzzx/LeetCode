d73ef21b038516e8fbe0398461f52c2e7cee4e55
wolfbloodzzx <zhangzi_xing@126.com>
2018-03-10 11:54:43 +0800

    刷了两道题

---
+++ src/jianzhi/RebuildBiTree.java
@@ -0,0 +1,17 @@
+package jianzhi;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class RebuildBiTree {
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+
+    }
+}
---
+++ src/leetcode/code669.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * Definition for a binary tree node.
+ * public class leetcode.TreeNode {
+ *     int val;
+ *     leetcode.TreeNode left;
+ *     leetcode.TreeNode right;
+ *     leetcode.TreeNode(int x) { val = x; }
+ * }
+ */
+public class code669 {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        return new TreeNode(1);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
